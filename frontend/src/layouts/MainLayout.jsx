43ea2afea5903de25048f1a41d76effb25153d2d
VicKituku <victorkituku@gmail.com>
2024-05-29 03:59:20 +0300

    added vercel analytics

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import { Analytics } from "@vercel/analytics/react";
 
 const MainLayout = () => {
   return (
     <>
+      <Analytics />
       {/* <NavBar /> */}
       <Outlet />
     </>
