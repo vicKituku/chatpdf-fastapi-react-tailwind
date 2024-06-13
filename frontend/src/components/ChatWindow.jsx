882b30efa205310836bb2b1296958f83dcb105f4
kituku <victorkituku@gmail.com>
2024-06-13 05:46:23 +0300

    Update ChatWindow.jsx
    change ip address

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -12,7 +12,7 @@ const ChatWindow = ({ namespace }) => {
     setMessages([...messages, userMessage]);
 
     try {
-      const response = await axios.post("https://13.48.6.219/query", {
+      const response = await axios.post("https://16.16.77.46/query", {
         query: message,
         namespace: namespace.toString(), // Replace with actual namespace
       });
